9aab9c3d4a6edf03fb456a87fafb5bc73160055d
Sumit Saurav <kaku.saurav@gmail.com>
2023-08-18 09:38:41 +0530

    Altered javascript practice file index.js with practice codes

--- javascript/index.js
+++ javascript/index.js
@@ -1,58 +1,56 @@
-// function fizzBuzz() {
+function fizzBuzz() {
     
-//  for (var a = 1;a <= 100; a++) {
+ for (var a = 1;a <= 100; a++) {
     
-//     if (a % 3 === 0 && a % 5 === 0) {
-//         console.log("FizzBuzz");
-//     }
-//     else if (a % 3 === 0 && a % 5 !== 0) {
-//       console.log("Fizz");
-//     } else if (a % 5 === 0 && a % 3 !== 0) {
-//       console.log("Buzz");
-//     } else {
-//         console.log(a);
-//     }
+    if (a % 3 === 0 && a % 5 === 0) {
+        console.log("FizzBuzz");
+    }
+    else if (a % 3 === 0 && a % 5 !== 0) {
+      console.log("Fizz");
+    } else if (a % 5 === 0 && a % 3 !== 0) {
+      console.log("Buzz");
+    } else {
+        console.log(a);
+    }
     
     
-//   }
-// }
-// fizzBuzz();
+  }
+}
 
-// var output = [];
-// output.push(1);
-// output.push(2);
-// output.push(3);
-// var popitem = output.pop();
+var output = [];
+output.push(1);
+output.push(2);
+output.push(3);
+var popitem = output.pop();
 
-// console.log(output);
-// console.log(popitem);
-// var names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
-// function whosPaying(names) {
+console.log(output);
+console.log(popitem);
 
-//     /******Don't change the code above*******/
+var names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+function whosPaying(names) {
 
-//         //Write your code here.
-//         var randomName = Math.floor(Math.random()*(names.length));
-//         var nameGenerated = (names[randomName]);
-//         console.log(nameGenerated + " is going to buy lunch today!");
-//         /******Don't change the code below*******/
-//     }
-//     whosPaying(names);
+    /******Don't change the code above*******/
 
-// 
+    
+        var randomName = Math.floor(Math.random()*(names.length));
+        var nameGenerated = (names[randomName]);
+        console.log(nameGenerated + " is going to buy lunch today!");
+        /******Don't change the code below*******/
+    }
+    whosPaying(names);
 
-// var numberOfBottles = 99
-// while (numberOfBottles >= 0) {
-//     var bottleWord = "bottle";
-//     if (numberOfBottles === 1) {
-//         bottleWord = "bottles";
-//     } 
-//     console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
-//     console.log(numberOfBottles + " " + bottleWord + " of beer,");
-//     console.log("Take one down, pass it around,");
-// 	numberOfBottles--;
-//     console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
-// }
+var numberOfBottles = 99
+while (numberOfBottles >= 0) {
+    var bottleWord = "bottle";
+    if (numberOfBottles === 1) {
+        bottleWord = "bottles";
+    } 
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+    console.log(numberOfBottles + " " + bottleWord + " of beer,");
+    console.log("Take one down, pass it around,");
+	numberOfBottles--;
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+}
 
 function fibonacciGenerator(n){
 var series =[];
@@ -78,18 +76,18 @@ fibonacciGenerator(10);
 
     
 
-// var output=[];
-// output.push(10);
-// output.push(11);
-// output.push(12);
-// output.push(13);
+var output1=[];
+output1.push(10);
+output1.push(11);
+output1.push(12);
+output1.push(13);
 
-// console.log(output);
-// console.log(output[1+1]);
-// console.log(output[3]);
-// console.log(output[1]);
+console.log(output1);
+console.log(output1[1+1]);
+console.log(output1[3]);
+console.log(output1[1]);
 
-// var x=output[1];
-// var y=output[1+1];
-// var z=output[1+1+1];
-// console.log(x,y,z);
+var x=output1[1];
+var y=output1[1+1];
+var z=output1[1+1+1];
+console.log(x,y,z);
